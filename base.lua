7d1e6d3ed9bfda37596561d44508cb60b2d1267e
Liam <liamreadsspam@gmail.com>
2021-09-07 00:29:23 -0300

    More setup

---
+++ base.lua
@@ -0,0 +1,44 @@
+local Object = require "source.lib.classic"
+local flux = require "source.lib.flux"
+
+local Base = Object:extend()
+
+function Base:new(x, y)
+    self.position = Vec2(x, y)
+    self.drawn_position = TILE_WIDTH * self.position
+    self.moving = false
+    self.alive = true
+end
+
+function Base:tick()
+end
+
+function Base:move(new_position)
+    if self.position == new_position then return end
+
+    if self.moving then
+        self.drawn_position = self.position * TILE_WIDTH
+    end
+
+    self.position = new_position
+    local new_drawn_position = new_position * TILE_WIDTH
+    flux.to(self.drawn_position, 0.2, {x = new_drawn_position.x, y = new_drawn_position.y}):oncomplete(function() self.moving = false end)
+    self.moving = true
+end
+
+function Base:undo(other)
+    self:move(other.position)
+    self.alive = other.alive
+end
+
+function Base:copy()
+    local copy = Base(self.position.x, self.position.y)
+    copy.alive = self.alive
+    return copy
+end
+
+function Base:__tostring()
+    return self.position:__tostring()
+end
+
+return Base
--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 require("lib.batteries"):export()
+Vec2 = require "lib.vec2"
+TILE_WIDTH = 16
 
 function love.update(dt) 
 end
