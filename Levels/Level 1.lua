a9599a24a6ada8ee1f25381964c0402a2e978ecd
cicetil <58564315+cicetil@users.noreply.github.com>
2021-09-06 22:25:02 -0400

    Level 1

---
+++ Levels/Level 1.lua
@@ -0,0 +1,92 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.7.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 14,
+  height = 7,
+  tilewidth = 16,
+  tileheight = 16,
+  nextlayerid = 3,
+  nextobjectid = 13,
+  properties = {},
+  tilesets = {
+    {
+      name = "Buzzle Tiles 1",
+      firstgid = 1,
+      filename = "Buzzle Tiles 1.tsx"
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 14,
+      height = 7,
+      id = 1,
+      name = "Tile Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 31, 31, 31, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 31, 41, 31, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 31, 57, 41, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 31, 57, 57, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 41, 57, 57, 57, 57, 57, 57, 57, 57,
+        57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 6,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 80,
+          y = 80,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 38,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 7,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 112,
+          y = 48,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 28,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
