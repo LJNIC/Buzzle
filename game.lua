3aed6ef847e168068525c673a48d3498a740fa50
Liam <liamreadsspam@gmail.com>
2021-09-10 00:24:37 -0300

    Scaling level

--- game.lua
+++ game.lua
@@ -6,30 +6,21 @@ local Level = require "level"
 local game = {}
 local uiImage = love.graphics.newImage("assets/UI.png")
 local interfaceScale = 4
-local gameScale = 4
+local gameScale = 2
 
 function game:enter()
     self.level = Level("levels.level1")
-    self.root = Plan.new()
-
-    local gameRules = Rules.new()
-        :addX(Plan.center())
-
-    self.gameContainer = Container:new(gameRules)
-
-    self.root:addChild(self.gameContainer)
 end
 
 function game:update(dt)
-    self.root:update(dt)
 end
 
 function game:draw()
     self.level:draw()
+    love.graphics.setBackgroundColor(29/255, 16/255, 28/255)
 
     love.graphics.push()
-        love.graphics.scale(gameScale, gameScale)
-        love.graphics.draw(self.level.canvas, 48)
+        love.graphics.draw(self.level.canvas, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, gameScale, gameScale, self.level.canvas:getWidth()/2, self.level.canvas:getWidth()/2)
     love.graphics.pop()
     love.graphics.push()
         love.graphics.scale(interfaceScale, interfaceScale)
